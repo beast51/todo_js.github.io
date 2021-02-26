df23348b7f12c1ee31abe1ad023cec0926009dc5
Zveryanskiy Yurii <47743786+beast51@users.noreply.github.com>
2021-02-26 13:54:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let todoList = getTodoFromLS();
+
+const input = document.querySelector(".todo__input");
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && input.value.trim() !== '') {
+    const item = {};
+    item.id = Date.now();
+    item.text = input.value;
+    item.completed = false;
+    input.value = "";
+    todoList.push(item);
+    renderTodoItem(todoList);
+    saveTodoToLS();
+  }
+});
+
+function renderTodoItem(array) {
+  const out = document.querySelector(".todo-out__items");
+  const outTitle = document.querySelector('.todo-out__title');
+  outTitle.innerHTML = `${todoList.length > 0 ? 'Список дел:' : 'Запланируй что-то)'}`
+  out.innerHTML = "";
+  array.forEach(
+    (item) =>
+      (out.innerHTML += `
+      <li class="todo-out__item item">
+      <label class="item-input">
+          <input 
+          class="item-input__checkbox" 
+          type="checkbox" id=${item.id} 
+          ${ item.completed ? "checked" : "" }
+          />
+          <span class="item-input__text">${item.text}<span> 
+      </label>
+      <button onClick={deleteTodoItem(event)} class="item-input__button" id=${item.id }>Удалить</button> 
+    </li>
+    `)
+  );
+}
+
+function deleteTodoItem(event) {
+  todoList = todoList.filter(item => item.id != event.target.id);
+  saveTodoToLS();
+  renderTodoItem(todoList);
+}
+
+function saveTodoToLS() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+function getTodoFromLS() {
+  return JSON.parse(localStorage.getItem("todoList"));
+}
+
+renderTodoItem(todoList);
