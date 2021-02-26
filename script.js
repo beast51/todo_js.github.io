fca40edf179e57edaa2a8b01df185f3d8a8f1c20
Zveryanskiy Yurii <47743786+beast51@users.noreply.github.com>
2021-02-26 18:34:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ input.addEventListener("keydown", (event) => {
     todoList.push({ 
       id : Date.now(),
       text: input.value,
-      completed: false
+      completed: false,
+      edit: false
     });
     input.value = "";
     renderTodoItem(todoList);
@@ -29,7 +30,10 @@ function renderTodoItem(array) {
           type="checkbox" id=${item.id} 
           ${ item.completed ? "checked" : "" }
           />
-          <span class="item-input__text">${item.text}<span> 
+          ${ !item.edit ? 
+          `<span ondblclick={editTodoTextOn(event)} class="item-input__text">${item.text}<span> ` 
+          : 
+          `<input onkeydown={editTodoTextOff(event)} class="item-input__input" value="${item.text}"/> `}
       </label>
       <button onClick={deleteTodoItem(event)} class="item-input__button" id=${item.id }>Удалить</button> 
     </li>
@@ -54,4 +58,26 @@ function getTodoFromLS() {
   return JSON.parse(localStorage.getItem("todoList"));
 }
 
+function editTodoTextOn(event) {
+  todoList.forEach(item => {
+    if (item.id == event.target.previousElementSibling.id) {
+      item.edit = !item.edit;
+      saveTodoToLS();
+      renderTodoItem(todoList);
+    };
+  });  
+}
+
+function editTodoTextOff(event) {
+  if (event.key === "Enter" && document.querySelector(".item-input__input").value.trim() !== '') {
+  todoList.forEach(item => {
+    if (item.id == event.target.previousElementSibling.id) {
+      item.text = document.querySelector(".item-input__input").value;
+      item.edit= !item.edit;
+  }});
+    renderTodoItem(todoList);
+    saveTodoToLS();
+  }
+}
+  
 renderTodoItem(todoList);
