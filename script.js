03cdd9353331dce5a014283313f5b5f30c65e106
Zveryanskiy Yuriy <zveryanskiy@gmail.com>
2021-02-28 18:15:21 +0200

    add fix edit mode

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let todoList = getTodoFromLS();
 const input = document.querySelector(".todo__input");
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter" && input.value.trim() !== '') {
-    todoList.push({ 
-      id : Date.now(),
+    todoList.push({
+      id: Date.now(),
       text: input.value,
       completed: false,
       edit: false
@@ -28,14 +28,14 @@ function renderTodoItem(array) {
           <input 
           class="item-input__checkbox" 
           type="checkbox" id=${item.id} 
-          ${ item.completed ? "checked" : "" }
+          ${item.completed ? "checked" : ""}
           />
-          ${ !item.edit ? 
-          `<span ondblclick={editTodoTextOn(event)} class="item-input__text">${item.text}<span> ` 
-          : 
-          `<input onkeydown={editTodoTextOff(event)} class="item-input__input" value="${item.text}"/> `}
+          ${!item.edit ?
+        `<span ondblclick={editTodoTextOn(event)} class="item-input__text">${item.text}<span> `
+        :
+        `<input onblur={editTodoTextOff(event)} class="item-input__input" value="${item.text}"/> `}
       </label>
-      <button onClick={deleteTodoItem(event)} class="item-input__button" id=${item.id }>Удалить</button> 
+      <button onClick={deleteTodoItem(event)} class="item-input__button" id=${item.id}>Удалить</button> 
     </li>
     `)
   );
@@ -64,20 +64,35 @@ function editTodoTextOn(event) {
       item.edit = !item.edit;
       saveTodoToLS();
       renderTodoItem(todoList);
+      document.querySelector('.item-input__input').focus();
+      document.querySelector('.item-input__input').onkeydown = function(event) {
+        if (event.key == 'Enter') {
+          this.blur();
+        }
+      };
     };
-  });  
+  });
 }
 
 function editTodoTextOff(event) {
-  if (event.key === "Enter" && document.querySelector(".item-input__input").value.trim() !== '') {
-  todoList.forEach(item => {
-    if (item.id == event.target.previousElementSibling.id) {
-      item.text = document.querySelector(".item-input__input").value;
-      item.edit= !item.edit;
-  }});
-    renderTodoItem(todoList);
+  if (document.querySelector(".item-input__input").value.trim() !== '') {
+    todoList.forEach(item => {
+      if (item.id == event.target.previousElementSibling.id) {
+        item.text = document.querySelector(".item-input__input").value;
+        item.edit = !item.edit;
+      }
+    });
     saveTodoToLS();
+    renderTodoItem(todoList);
+  } else {
+    todoList.forEach(item => {
+      if (item.id == event.target.previousElementSibling.id) {
+        item.edit = !item.edit;
+      }
+    });
+    saveTodoToLS();
+    renderTodoItem(todoList);
   }
 }
-  
+
 renderTodoItem(todoList);
